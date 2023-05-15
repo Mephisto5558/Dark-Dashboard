50692cf3ce4840079136afe568a77c280c7bb71a
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2023-05-15 14:51:49 +0200

    set 1

---
+++ src/perfect_scrollbar.js
@@ -0,0 +1,39 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+function myFunction() {
+    var element = document.getElementById("perfect-scrollbar-on");
+    element.classList.toggle("nav-open");
+}
+
+}
+/*
+     FILE ARCHIVED ON 08:51:52 Jan 03, 2023 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 19:15:08 May 14, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 1229.253
+  exclusion.robots: 0.079
+  exclusion.robots.policy: 0.066
+  RedisCDXSource: 0.611
+  esindex: 0.011
+  LoadShardBlock: 1209.92 (3)
+  PetaboxLoader3.datanode: 120.09 (5)
+  load_resource: 687.948
+  PetaboxLoader3.resolve: 603.936
+  loaddict: 32.539
+*/
